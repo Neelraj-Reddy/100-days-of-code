9a21aaa3ab48e99406077a6d57b5135add8b1e52
Neelraj-Reddy <neelrajreddy1694@gmail.com>
2024-03-24 19:32:04 +0530

    success

---
+++ day 1 to 10/day 4/avoid_squares.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N;
+	    cin>>N;
+	    for(int i=1;i<=N;i++){
+	        cout<<i<<" ";
+	    }
+	    cout<<endl;
+	}
+
+}
